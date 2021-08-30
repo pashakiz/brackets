f48f5a39888c27c4c35d8549578bda5771565ecb
pashakiz <pashakiz@gmail.com>
2021-08-30 19:10:59 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check = (str, bracketsConfig) => {
+  let brackets = bracketsConfig.reduce((acc, el) => (acc[el[0]] = el[1], acc), {})
+  let stack = []
+  for (let item of str) {
+    if (item === brackets[stack[stack.length-1]]) {
+      stack.pop()
+      continue
+    }
+    if (brackets[item]) {
+      stack.push(item)
+      continue
+    }
+    return false
+  }
+  return !stack.length
 }
